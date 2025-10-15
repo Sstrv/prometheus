aecd52e771f71970f43f590757abede73f39f3c7
vespindolaaldana-rgb <vespindolaaldana@gmail.com>
2025-10-15 16:10:45 -0500

    .

--- Vuelos/src/main/java/com/uptc/frw/vuelos/repository/CreditCard.java
+++
@@ -1,6 +0,0 @@
-package com.uptc.frw.vuelos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditCard extends JpaRepository<CreditCard,Long> {
-}
---
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/service/CreditCardService.java
@@ -0,0 +1,4 @@
+package com.uptc.frw.vuelos.service;
+
+public class CreditCardService {
+}
