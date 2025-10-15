ca3417e37da6328a5853a582271e97a7ec223d99
inda- <idbustoscu@unal.edu.co>
2025-10-15 16:18:04 -0500

    Seat y Airplane repositorios

---
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/repository/SeatRepository.java
@@ -0,0 +1,7 @@
+package com.uptc.frw.vuelos.repository;
+
+import com.uptc.frw.vuelos.model.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatRepository extends JpaRepository<Seat,Long> {
+}
