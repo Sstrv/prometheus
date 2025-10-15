e5583a3686fb2a14acc4ad771fa2c04afc63abc6
Sstrv <116982899+Sstrv@users.noreply.github.com>
2025-10-15 11:40:32 -0500

    Correccion de Sql en la tabla asientos

--- tablasVuelos.sql
+++ tablasVuelos.sql
@@ -1,3 +1,4 @@
+drop user vuelos cascade constraint;
 create user vuelos identified by vuelos default tablespace USERS temporary tablespace TEMP;
 GRANT CREATE SESSION, CREATE TABLE, CREATE VIEW, CREATE SEQUENCE, CREATE SYNONYM, CREATE PROCEDURE, CREATE TRIGGER 
 TO vuelos;
