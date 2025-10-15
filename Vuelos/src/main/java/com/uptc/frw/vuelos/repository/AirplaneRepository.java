38ee667ff4bee497524d8ca417c3b1f78d14128e
Esteban Martinez <estebanmz@rva.lat>
2025-10-15 16:14:55 -0500

    Crear AirplaneRepository.java y FlightRepository.java

---
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/repository/AirplaneRepository.java
@@ -0,0 +1,8 @@
+package com.uptc.frw.vuelos.repository;
+
+import com.uptc.frw.vuelos.model.Airplane;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AirplaneRepository extends JpaRepository<Airplane, Long> {
+
+}
