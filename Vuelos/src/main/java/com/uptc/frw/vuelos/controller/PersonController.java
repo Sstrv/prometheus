e87c2333f328c8af137a0fce3bded6a4e98dd96d
Sstrv <116982899+Sstrv@users.noreply.github.com>
2025-10-16 08:58:59 -0500

    Airport, Airport service

--- Vuelos/src/main/java/com/uptc/frw/vuelos/controller/PersonController.java
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/controller/PersonController.java
@@ -6,7 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonService personService;
