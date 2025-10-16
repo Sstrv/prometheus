8e24df2685aa1578dd026b808d7983bc06118dd6
vespindolaaldana-rgb <vespindolaaldana@gmail.com>
2025-10-16 13:42:15 -0500

    CreditCard controller

--- Vuelos/src/main/java/com/uptc/frw/vuelos/controller/CreditCardController.java
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/controller/CreditCardController.java
@@ -1,10 +1,35 @@
 package com.uptc.frw.vuelos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.uptc.frw.vuelos.model.CreditCard;
+import com.uptc.frw.vuelos.service.CreditCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("creditcard")
 public class CreditCardController {
-//hola
+    @Autowired
+    private CreditCardService creditCardService;
+
+    public List<CreditCard> getall(){
+        return creditCardService.findAllCreditCard();
+    }
+    @GetMapping("/{id}")
+    public CreditCard getCreditCard(@PathVariable long id){
+        return creditCardService.getCreditCardById(id);
+    }
+    @PostMapping
+    public CreditCard SaveCreditCard(@RequestBody CreditCard creditCard){
+        return creditCardService.saveCreditCard(creditCard);
+    }
+    @PutMapping
+    public CreditCard updateCreditCard(@RequestBody CreditCard creditCard){
+        return creditCardService.updateCreditCard(creditCard);
+    }
+    @DeleteMapping
+    public void deleteCreditCard(@RequestBody Long id){
+        creditCardService.deleteCreditCard(id);
+    }
 }
