7686fca3c21b1e01de9bdc9306cb81a8554c6c14
Esteban Martinez <estebanmz@rva.lat>
2025-10-16 10:13:54 -0500

    Crear FlightService.java
    Arreglo un error en SeatService.java

--- Vuelos/src/main/java/com/uptc/frw/vuelos/service/SeatService.java
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/service/SeatService.java
@@ -2,6 +2,7 @@ package com.uptc.frw.vuelos.service;
 
 import com.uptc.frw.vuelos.model.Seat;
 import com.uptc.frw.vuelos.repository.SeatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Service
 public class SeatService {
 
+    @Autowired
     private SeatRepository seatRepository;
 
     public List<Seat> findAllSeats(){
