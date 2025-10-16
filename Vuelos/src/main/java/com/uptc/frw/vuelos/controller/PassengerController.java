ff009df5d9d6ff64f79599e764df7c23d1117389
inda- <idbustoscu@unal.edu.co>
2025-10-16 15:15:54 -0500

    creacion de PassengerController

---
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/controller/PassengerController.java
@@ -0,0 +1,39 @@
+package com.uptc.frw.vuelos.controller;
+
+import com.uptc.frw.vuelos.model.Passenger;
+import com.uptc.frw.vuelos.service.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/passengerControler")
+public class PassengerController {
+    @Autowired
+    private PassengerService passengerService;
+
+    @GetMapping
+    public List<Passenger>getAll(){
+        return passengerService.findAllPassengers();
+    }
+    @GetMapping("/{id}")
+    public Passenger getPassenger(@PathVariable long id){
+        return passengerService.getPassengerById(id);
+    }
+
+    @PutMapping
+    public Passenger savePassenger(@RequestBody Passenger passenger){
+        return passengerService.savePassenger(passenger);
+    }
+
+    @PutMapping
+    public Passenger updatePasssenger(@RequestBody Passenger passenger){
+        return passengerService.updatePassenger(passenger);
+    }
+
+    @DeleteMapping
+    public void deletePassenger(@RequestParam Long id){
+        passengerService.deletePassenger(id);
+    }
+}
