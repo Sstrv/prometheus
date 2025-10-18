c336442c40289decb9e55ba08aff3f0e44c5477f
Esteban Martinez <estebanmz@rva.lat>
2025-10-17 22:45:22 -0500

    Delete function in PersonRepository
    wth is doing a polar bear in arlington, texas xdddd

--- Vuelos/src/main/java/com/uptc/frw/vuelos/repository/PersonRepository.java
+++ Vuelos/src/main/java/com/uptc/frw/vuelos/repository/PersonRepository.java
@@ -4,5 +4,4 @@ import com.uptc.frw.vuelos.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person,Long> {
-    void deleteById(long id);
 }
